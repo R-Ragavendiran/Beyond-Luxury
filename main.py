d12162421bd86c8dd105b35d2d88de875b325c9f
R-Ragavendiran <114816583+R-Ragavendiran@users.noreply.github.com>
2022-10-22 19:23:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,391 @@
+print("Welcome to Beyond Luxury")
+print("Select the place you want to vist")
+
+print("")  # This is for free space
+
+print("1.Austria")
+print("2.Singapore")
+print("3.China")
+print("4.Spain")
+print("5.Italy")
+print("6.New Zealand")
+print("7.Denmark")
+print("8.India")
+
+print("")  # This is for free space
+# This is first place
+
+USER = int(input("Enter the place number you want to vist :"))
+if USER == 1:
+    print("you are selected to vist Austria")
+    print("$21,000 is the cost for a trip around the Austria for a person")
+    print("")  # This is for free space
+
+    print("this are places you are visiting in this trip")
+    print("1.Lone Pine Koala Sanctuary")
+    print("2.Sydney Opera House")
+    print("3.Royal Botanic Gardens Victoria.")
+    print("4.Cairns Esplanade Lagoon")
+    print("5.Kings Park and Botanic Garden")
+    print("6.Adelaide Central Market")
+    print("7.Mount Wellington")
+    print("8.Australian War Memoria")
+
+    print("")  # This is for free space
+    print("If you need only one place ")
+    user1 = int(input("enter the place number you want vist :"))
+    if user1 == 1:
+        print("you are selected Lone Pine Koala Sanctuary")
+        print("the cost is to vist this this place :3000")
+    elif user1 == 2:
+        print("you are selected Sydney Opera House")
+        print("the cost is to vist this place :3000")
+    elif user1 == 3:
+        print("you are selected Royal Botanic Gardens Victoria")
+        print("the cost is to vist this place :3000")
+    elif user1 == 4:
+        print("you are selected Cairns Esplanade Lagoon")
+        print("the cost is to vist this place :3000")
+    elif user1 == 5:
+        print("you are selected Kings Park and Botanic Garden")
+        print("the cost is to vist this place :3000")
+    elif user1 == 6:
+        print("you are selected Adelaide Central Market")
+        print("the cost is to vist this place :3000")
+    elif user1 == 7:
+        print("you are selected Mount Wellington")
+        print("the cost is to vist this place :3000")
+    elif user1 == 8:
+        print("you are selected Australian War Memoria")
+        print("the cost is to vist this place :3000")
+    else:
+        print("enter the correct number")
+
+
+# This is the 2 place
+if USER == 2:
+    print("you are selected to vist Singapore")
+    print("$52,000 is the cost for a trip around the Singapore for a person")
+    print("")
+    print("this are places you are visiting in this trip")
+    print("1.Gardens by the Bay")
+    print("2.Singapore Botanic Gardens")
+    print("3.Singapore Zoo")
+    print("4.National Orchid Garden.")
+    print("5.Cloud Forest.")
+    print("6.Sands Skypark Observation Deck.")
+    print("7.Singapore Mass Rapid Transit.")
+    print("8.Singapore Flye.")
+
+    print("")  # This is for free space
+
+    print("If you need only one place ")
+    user2 = int(input("enter the place number you want vist :"))
+    if user2 == 1:
+        print("you are selected Gardens by the Bay")
+        print("the cost is to vist this this place :6,500")
+    elif user2 == 2:
+        print("you are selected Singapore Botanic Gardens")
+        print("the cost is to vist this place :6,500")
+    elif user2 == 3:
+        print("you are selected Singapore Zoo")
+        print("the cost is to vist this place :6,500")
+    elif user2 == 4:
+        print("you are selected National Orchid Garden.")
+        print("the cost is to vist this place :6,500")
+    elif user2 == 5:
+        print("you are selected Cloud Forest.")
+        print("the cost is to vist this place :6,500")
+    elif user2 == 6:
+        print("you are selected Sands Skypark Observation Deck.")
+        print("the cost is to vist this place :6,500")
+    elif user2 == 7:
+        print("you are selected Singapore Mass Rapid Transit.")
+        print("the cost is to vist this place :6,500")
+    elif user2 == 8:
+        print("you are selected Singapore Flye.")
+        print("the cost is to vist this place :6,500")
+    else:
+        print("enter the correct number")
+
+# This is th third place
+
+print("")  # This is for free space
+
+if USER == 3:
+    print("you are selected to vist China")
+    print("$45,000 is the cost for a trip around the China for a person")
+    print("this are places you are visiting in this trip")
+
+    print("1.The Great Wall Of China")
+    print("2.Forbidden City")
+    print("3.Imperial Palace")
+    print("4.The Terracotta Army, Xi'an")
+    print("5.The Summer Palace")
+    print("6.Li River, Guilin")
+    print("7.Chengdu Research Base of Giant Panda Breeding")
+    print("8.Yangtze River and the Three Gorges")
+
+    print("")  # This is for free space
+
+    print("If you need only one place ")
+    user3 = int(input("enter the place number you want vist :"))
+    if user3 == 1:
+        print("you are selected The Great Wall Of China")
+        print("the cost is to vist this this place :5,625")
+    elif user3 == 2:
+        print("you are selected Forbidden City")
+        print("the cost is to vist this place :5,625")
+    elif user3 == 3:
+        print("you are selected Imperial Palace")
+        print("the cost is to vist this place :5,625")
+    elif user3 == 4:
+        print("you are selected The Terracotta Army, Xi'an")
+        print("the cost is to vist this place :5,625")
+    elif user3 == 5:
+        print("you are selected The Summer Palace")
+        print("the cost is to vist this place :5,625")
+    elif user3 == 6:
+        print("you are selected Li River, Guilin.")
+        print("the cost is to vist this place :5,625")
+    elif user3 == 7:
+        print("you are selected Chengdu Research Base of Giant Panda Breeding")
+        print("the cost is to vist this place :5,625")
+    elif user3 == 8:
+        print("you are selected Yangtze River and the Three Gorges")
+        print("the cost is to vist this place :5,625")
+    else:
+        print("enter the correct number")
+
+    # This is the 4 place
+if USER == 4:
+    print("you are selected to vist Spain")
+    print("$55,000 is the cost for a trip around the Singapore for a person")
+    print("")
+    print("this are places you are visiting in this trip")
+    print("1.Madrid")
+    print("2.Barcelona")
+    print("3.San Sebastián")
+    print("4.Balearic islands")
+    print("5.Seville")
+    print("6.Toledo")
+    print("7.Santiago de Compostela")
+    print("8.Granada")
+
+    print("")  # This is for free space
+
+    print("If you need only one place ")
+    user4 = int(input("enter the place number you want vist :"))
+    if user4 == 1:
+        print("you are selected Madrid")
+        print("the cost is to vist this this place :6,875")
+    elif user4 == 2:
+        print("you are selected Barcelona")
+        print("the cost is to vist this place :6,875")
+    elif user4 == 3:
+        print("you are selected San Sebastián")
+        print("the cost is to vist this place :6,875")
+    elif user4 == 4:
+        print("you are selected Balearic islands ")
+        print("the cost is to vist this place :6,875")
+    elif user4 == 5:
+        print("you are selected Seville")
+        print("the cost is to vist this place :6,875")
+    elif user4 == 6:
+        print("you are selected Seville")
+        print("the cost is to vist this place :6,875")
+    elif user4 == 7:
+        print("you are selected Toledo")
+        print("the cost is to vist this place :6,875")
+    elif user4 == 8:
+        print("you are selected Santiago de Compostela")
+        print("the cost is to vist this place :")
+    else:
+        print("enter the correct number")
+
+# This is the 5 place
+if USER == 5:
+    print("you are selected to vist Italy")
+    print("$35,000 is the cost for a trip around the Singapore for a person")
+    print("")
+    print("this are places you are visiting in this trip")
+    print("1.Rome")
+    print("2.Florence")
+    print("3.Tuscany")
+    print("4.Siena")
+    print("5.Milan")
+    print("6.Lake Como")
+    print("7.Venice")
+    print("8.Cinque Terre")
+
+    print("")  # This is for free space
+
+    print("If you need only one place ")
+    user5 = int(input("enter the place number you want vist :"))
+    if user5 == 1:
+        print("you are selected Rome")
+        print("the cost is to vist this this place :4,375")
+    elif user5 == 2:
+        print("you are selected Florence")
+        print("the cost is to vist this place :4,375")
+    elif user5 == 3:
+        print("you are selected Tuscany")
+        print("the cost is to vist this place :4,375")
+    elif user5 == 4:
+        print("you are selected Siena ")
+        print("the cost is to vist this place :4,375")
+    elif user5 == 5:
+        print("you are selected Milan")
+        print("the cost is to vist this place :4,375")
+    elif user5 == 6:
+        print("you are selected Lake Como.")
+        print("the cost is to vist this place :4,375")
+    elif user5 == 7:
+        print("you are selected Venice")
+        print("the cost is to vist this place :4,375")
+    elif user5 == 8:
+        print("you are selected Cinque Terre")
+        print("the cost is to vist this place :4,375")
+    else:
+        print("enter the correct number")
+
+    # This is the 6 place
+if USER == 6:
+    print("you are selected to vist New-Zealand")
+    print("$48,500 is the cost for a trip around the Singapore for a person")
+    print("")
+    print("this are places you are visiting in this trip")
+    print("1.Queenstown")
+    print("2.Arrowtown")
+    print("3.Rotorua")
+    print("4.Milford Sound")
+    print("5.Wanaka.")
+    print("6.Waiheke Island")
+    print("7.Franz Josef Glacier")
+    print("8.Aoraki/Mount Cook National Park.")
+
+    print("")  # This is for free space
+
+    print("If you need only one place ")
+    user6 = int(input("enter the place number you want vist :"))
+    if user6 == 1:
+        print("you are selected Queenstown")
+        print("the cost is to vist this this place :6,062")
+    elif user6 == 2:
+        print("you are selected Arrowtown")
+        print("the cost is to vist this place :6,062")
+    elif user6 == 3:
+        print("you are selected Rotorua.")
+        print("the cost is to vist this place :6,062")
+    elif user6 == 4:
+        print("you are selected  Milford Sound")
+        print("the cost is to vist this place :6,062")
+    elif user6 == 5:
+        print("you are selected Wanaka")
+        print("the cost is to vist this place :6,062")
+    elif user6 == 6:
+        print("you are selected Waiheke Island")
+        print("the cost is to vist this place :6,062")
+    elif user6 == 7:
+        print("you are selected Franz Josef Glacier")
+        print("the cost is to vist this place :6,062")
+    elif user6 == 8:
+        print("you are selected Aoraki/Mount Cook National Park.")
+        print("the cost is to vist this place :6,062")
+    else:
+        print("enter the correct number")
+
+    # This is the 7 place
+if USER == 7:
+    print("you are selected to vist Denmark ")
+    print("$38,500 is the cost for a trip around the Singapore for a person")
+    print("")
+    print("this are places you are visiting in this trip")
+    print("1.Nyhavn.")
+    print("2.Tivoli Gardens.")
+    print("3.Den Gamle")
+    print("4.LEGOLAND Billund")
+    print("5.LEGO House. ")
+    print("6.Romo Island")
+    print("7.Rosenborg Castle")
+    print("8.Egeskov Castle.")
+
+    print("")  # This is for free space
+
+    print("If you need only one place ")
+    user7 = int(input("enter the place number you want vist :"))
+    if user7 == 1:
+        print("you are selected Nyhavn")
+        print("the cost is to vist this this place :4,812")
+    elif user7 == 2:
+        print("you are selected Tivoli Gardens")
+        print("the cost is to vist this place :4,812")
+    elif user7 == 3:
+        print("you are selected Den Gamle")
+        print("the cost is to vist this place :4,812")
+    elif user7 == 4:
+        print("you are selected LEGOLAND Billund ")
+        print("the cost is to vist this place :4,812")
+    elif user7 == 5:
+        print("you are selected LEGO House")
+        print("the cost is to vist this place :4,812")
+    elif user7 == 6:
+        print("you are selected Romo Island")
+        print("the cost is to vist this place :4,812")
+    elif user7 == 7:
+        print("you are selected Rosenborg Castle")
+        print("the cost is to vist this place :4,812")
+    elif user7 == 8:
+        print("you are selected Egeskov Castle")
+        print("the cost is to vist this place :4,812")
+    else:
+        print("enter the correct number")
+
+    # This is the 8 place
+if USER == 8:
+    print("you are selected to vist India ")
+    print("$80,000 is the cost for a trip around the Singapore for a person")
+    print("")
+    print("this are places you are visiting in this trip")
+    print("1.Agra")
+    print("2.Jaipur")
+    print("3.Alleppey")
+    print("4.Varanasi ")
+    print("5.Delhi")
+    print("6.Udaipur")
+    print("7.Pondicherry")
+    print("8.Bangalor")
+
+    print("")  # This is for free space
+
+    print("If you need only one place ")
+    user8 = int(input("enter the place number you want vist :"))
+    if user8 == 1:
+        print("you are selected Agra")
+        print("the cost is to vist this this place :10000")
+    elif user8 == 2:
+        print("you are selected Jaipur")
+        print("the cost is to vist this place :10000")
+    elif user8 == 3:
+        print("you are selected Alleppey")
+        print("the cost is to vist this place :10000")
+    elif user8 == 4:
+        print("you are selected  Varanasi")
+        print("the cost is to vist this place :10000")
+    elif user8 == 5:
+        print("you are selected Delhi ")
+        print("the cost is to vist this place :10000")
+    elif user8 == 6:
+        print("you are selected Udaipur ")
+        print("the cost is to vist this place :10000")
+    elif user8 == 7:
+        print("you are selected Udaipur ")
+        print("the cost is to vist this place :10000")
+    elif user8 == 8:
+        print("you are selected Bangalor")
+        print("the cost is to vist this place :10000")
+    else:
+        print("enter the correct number")
+
+if USER>=9:
+    print("Enter The Correct Place Number")
